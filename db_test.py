2073b4dd1ff47f0c6c76cb4342c5e2ef00a6a4a6
isabelbrechin <isymaeve@gmail.com>
2024-03-31 16:30:50 -0300

    Update db_test.py
    Editing db_test.py to make it more simple

--- db_test.py
+++ db_test.py
@@ -4,14 +4,9 @@ connection = sqlite3.connect('database.db')
 
 cur = connection.cursor()
 
-print("Feedback entries:")
-feedback_entries = cur.execute("SELECT * FROM feedback").fetchall()
-for entry in feedback_entries:
-    print(entry)
+print(cur.execute("SELECT * FROM feedback").fetchall())
 
-print("\nAnalysis entries:")
-analysis_entries = cur.execute("SELECT * FROM analysis").fetchall()
-for entry in analysis_entries:
-    print(entry)
+print(cur.execute("SELECT * FROM analysis").fetchall())
 
+connection.commit()
 connection.close()
