aa243dbf1209c868f2d28e5de6e13b3f6d5648ce
isabelbrechin <isymaeve@gmail.com>
2024-03-31 16:01:06 -0300

    Update init_db.py
    Changing colins original file to fit our purposes

--- init_db.py
+++ init_db.py
@@ -8,8 +8,9 @@ with open('schema.sql') as f:
     script = f.read()
     cur.executescript(script)
 
-cur.execute("INSERT INTO researcher (ResearcherName, ResearcherEmail) VALUES (?, ?)", ('Test Researcher', 'Test.Researcher@dal.ca'))
-cur.execute("INSERT INTO prompts (ResearcherID, Consent, Msg) VALUES (?, ?, ?)", (1, 'This is my test form. It is great!', 'This is a test response. It is not as great as you thought.'))
+cur.execute("INSERT INTO feedback (content) VALUES (?)", ('This is a sample piece of feedback'))
+cur.execute("INSERT INTO analysis (sentiment, keywords, summary) VALUES (?, ?, ?)", ('postitive', 'keywordkeyword', 'This feedback is generally posisitive, here is a summary.'))
+
 
 
 connection.commit()
