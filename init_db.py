0dd466e44bc64320af8eaf12a8819997a5b333dc
isabelbrechin <isymaeve@gmail.com>
2024-04-03 12:35:48 -0300

    Update init_db.py
    Removing some spaces

--- init_db.py
+++ init_db.py
@@ -11,7 +11,5 @@ with open('schema.sql') as f:
 cur.execute("INSERT INTO feedback (content) VALUES (?)", ('This is a sample piece of feedback'))
 cur.execute("INSERT INTO analysis (sentiment, keywords, summary) VALUES (?, ?, ?)", ('postitive', 'keywordkeyword', 'This feedback is generally posisitive, here is a summary.'))
 
-
-
 connection.commit()
 connection.close()
