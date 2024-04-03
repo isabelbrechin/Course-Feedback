5afa2612f8c24af09321f86d8817669638b87aea
isabelbrechin <isymaeve@gmail.com>
2024-04-03 12:40:59 -0300

    Update init_db.py
    adding a comma- got another syntax error notice when initializing

--- init_db.py
+++ init_db.py
@@ -8,7 +8,7 @@ with open('schema.sql') as f:
     script = f.read()
     cur.executescript(script)
 
-cur.execute("INSERT INTO feedback (content) VALUES (?)", ('This is a sample piece of feedback'))
+cur.execute("INSERT INTO feedback (content) VALUES (?)", ('This is a sample piece of feedback',))
 cur.execute("INSERT INTO analysis (sentiment, keywords, summary) VALUES (?, ?, ?)", ('postitive', 'keywordkeyword', 'This feedback is generally posisitive, here is a summary.'))
 
 connection.commit()
